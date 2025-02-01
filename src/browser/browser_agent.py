8b059e6e52f47122f1b0ca656d8b4fb00fb9d0cb
heyitsaamir <ajawaid191@gmail.com>
2025-01-31 17:43:24 -0800

    Update fmt

--- src/browser/browser_agent.py
+++ src/browser/browser_agent.py
@@ -12,58 +12,69 @@ from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
 from browser.session import SessionStepState
 
-llm = AzureChatOpenAI(azure_endpoint=os.environ["AZURE_OPENAI_API_BASE"],
-    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
-    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],) if os.environ["AZURE_OPENAI_API_BASE"] else ChatOpenAI(
-    model="gpt-4o"
+llm = (
+    AzureChatOpenAI(
+        azure_endpoint=os.environ["AZURE_OPENAI_API_BASE"],
+        azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
+        openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+    )
+    if os.environ["AZURE_OPENAI_API_BASE"]
+    else ChatOpenAI(model=os.environ["OPENAI_MODEL_NAME"])
 )
 
-async def run_browser_agent(query: str, context: TurnContext):           
+
+async def run_browser_agent(query: str, context: TurnContext):
     def step_callback(state: BrowserState, output: AgentOutput, step_number: int):
         session = context.has("session") and context.get("session")
         io = context.has("socket") and context.get("socket")
         if session:
             # Extract planned actions
-            actions = [
-                action.model_dump_json(exclude_unset=True) 
-                for action in output.action
-            ] if output.action else []
-            
+            actions = (
+                [action.model_dump_json(exclude_unset=True) for action in output.action]
+                if output.action
+                else []
+            )
+
             step = SessionStepState(
                 screenshot=state.screenshot,
                 action=output.current_state.evaluation_previous_goal,
                 memory=output.current_state.memory,
                 next_goal=output.current_state.next_goal,
-                actions=actions
+                actions=actions,
             )
 
             session.session_state.append(step)
         else:
             print("No session")
-            
+
         if io and step:
-            asyncio.create_task(io.emit("message", {
-                "screenshot": step.screenshot,
-                "action": step.action,
-                "memory": step.memory,
-                "next_goal": step.next_goal,
-                "actions": step.actions
-            }))
+            asyncio.create_task(
+                io.emit(
+                    "message",
+                    {
+                        "screenshot": step.screenshot,
+                        "action": step.action,
+                        "memory": step.memory,
+                        "next_goal": step.next_goal,
+                        "actions": step.actions,
+                    },
+                )
+            )
         else:
             print("No socket")
-        
+
     agent = Agent(
         task=query,
         llm=llm,
         register_new_step_callback=step_callback,
         browser=Browser(
             config=BrowserConfig(
-                chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',  # macOS path
+                chrome_instance_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",  # macOS path
                 # For Windows, typically: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
                 # For Linux, typically: '/usr/bin/google-chrome'
-                headless=True
+                headless=True,
             )
-        )
+        ),
     )
     try:
         result = await agent.run()
@@ -72,4 +83,3 @@ async def run_browser_agent(query: str, context: TurnContext):
     except Exception as e:
         print(e)
         return "Ran into an error"
-        
